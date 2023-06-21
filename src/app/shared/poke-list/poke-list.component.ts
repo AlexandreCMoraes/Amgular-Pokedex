db075bd2e9f307f6d6fe361ad3531c3061e1c27a
AlexandreCMoraes <alexcmoraes@hotmail.com>
2023-06-21 19:03:40 -0300

    renderizando dados api e ajustes css

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -9,9 +9,15 @@ import { PokeApiService } from './../../service/poke-api.service';
   styleUrls: ['./poke-list.component.scss'],
 })
 export class PokeListComponent implements OnInit {
+  public getAllPokemons: any;
+
   constructor(private pokeApiService: PokeApiService) {}
   // assim que carrega a pagina, é mostrado as dados
   ngOnInit(): void {
-    this.pokeApiService.apiListAllPokemons.subscribe((res) => console.log(res));
+    this.pokeApiService.apiListAllPokemons.subscribe((res) => {
+      this.getAllPokemons = res.results;
+      console.log(`Com todos os detalhes (status)`, res);
+      console.log(`Todos os pokemons`, this.getAllPokemons);
+    });
   }
 }
