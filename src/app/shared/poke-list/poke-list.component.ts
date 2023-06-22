d33a2ed895cea3ca75c578a1d4a436fc5f90bbcd
AlexandreCMoraes <alexcmoraes@hotmail.com>
2023-06-21 21:03:34 -0300

    ok rota de pag details e ajus busca e css 2

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -9,19 +9,28 @@ import { PokeApiService } from './../../service/poke-api.service';
   styleUrls: ['./poke-list.component.scss'],
 })
 export class PokeListComponent implements OnInit {
+  private setAllPokemons: any;
   public getAllPokemons: any;
 
   constructor(private pokeApiService: PokeApiService) {}
   // assim que carrega a pagina, é mostrado as dados
   ngOnInit(): void {
     this.pokeApiService.apiListAllPokemons.subscribe((res) => {
-      this.getAllPokemons = res.results;
+      this.setAllPokemons = res.results;
+      this.getAllPokemons = this.setAllPokemons;
       // console.log(`Com todos os detalhes (status)`, res);
       // console.log(`Todos os pokemons`, this.getAllPokemons);
     });
   }
 
   public getSearch(value: string) {
-    console.log(value);
+    // console.log(value);
+    // filtra valores iguais aos passados
+    const filter = this.getAllPokemons.filter((res: any) => {
+      // verifica se igual  a api
+      return !res.name.indexOf(value.toLowerCase());
+    });
+    // aqui tem os dados cheio 
+    this.getAllPokemons = filter;
   }
 }
